6865dc32e385ccc48a7c759987376289582c54fb
omar9ismail <Omar0100!>
2021-08-19 16:57:34 +0200

    add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: MyHomePage(), title: 'Flutter Demo', theme: ThemeData());
+        home: MyHomePage2(), title: 'Flutter Demo', theme: ThemeData());
   }
 }
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   List<Movie> movies = [];
 
   @override
   void initState() {
-    getHttp();
     super.initState();
+    getHttp();
   }
 
   void getHttp() async {
@@ -58,8 +60,11 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('MoviesApp',style: TextStyle(color: Colors.cyanAccent,fontWeight: FontWeight.bold),
-      ),
+        title: Text(
+          'MoviesApp',
+          style:
+              TextStyle(color: Colors.cyanAccent, fontWeight: FontWeight.bold),
+        ),
         backgroundColor: Color(0xff3E2C41),
       ),
       body: ListView.builder(
@@ -102,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               children: [
                                 Expanded(
                                   child: Text(
-                                    movie.title??'',
+                                    movie.title ?? '',
                                     style: TextStyle(
                                         fontSize: 20,
                                         color: Colors.white,
@@ -116,8 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                             Row(children: [
                               RatingBar.builder(
-                                unratedColor:Colors.blueGrey
-,
+                                unratedColor: Colors.blueGrey,
                                 itemSize: 20,
                                 initialRating: 3,
                                 minRating: 1,
@@ -192,7 +196,11 @@ class Screen3 extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Color(0xff3E2C41),
-          title: Text(name ?? '',style: TextStyle(color: Colors.cyanAccent,fontWeight: FontWeight.bold),),
+          title: Text(
+            name ?? '',
+            style: TextStyle(
+                color: Colors.cyanAccent, fontWeight: FontWeight.bold),
+          ),
         ),
         body: Container(
           color: Color(0xff261C2C),
@@ -200,22 +208,29 @@ class Screen3 extends StatelessWidget {
             children: [
               Expanded(
                 child: Container(
-
                   decoration: BoxDecoration(
-          border: Border.all(color: Color(0xff5C527F),width: 4,),
-
-            image: DecorationImage(
-                          image: NetworkImage(
-                              'https://image.tmdb.org/t/p/original/${image}'),
-                          fit: BoxFit.fill,
+                      border: Border.all(
+                        color: Color(0xff5C527F),
+                        width: 4,
+                      ),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://image.tmdb.org/t/p/original/${image}'),
+                        fit: BoxFit.fill,
                       )),
                 ),
               ),
-              SizedBox(height: 10,),
-              Text(name ?? '',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),),
-              SizedBox(height: 15,),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                name ?? '',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15,
+              ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -225,12 +240,13 @@ class Screen3 extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 15,),
-              Row(crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+              SizedBox(
+                height: 15,
+              ),
+              Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
                 RatingBar.builder(
-              unratedColor:Colors.blueGrey
-                  ,itemSize: 20,
+                  unratedColor: Colors.blueGrey,
+                  itemSize: 20,
                   initialRating: 3,
                   minRating: 1,
                   direction: Axis.horizontal,
@@ -255,23 +271,196 @@ class Screen3 extends StatelessWidget {
                       color: Colors.white,
                       fontWeight: FontWeight.bold),
                 ),
-
               ]),
-              SizedBox(height: 15,),
+              SizedBox(
+                height: 15,
+              ),
               Text(
                 overview ?? '',
                 style: TextStyle(color: Colors.white, fontSize: 20),
               ),
-
             ],
-
           ),
         ));
   }
 }
 
+class MyHomePage2 extends StatefulWidget {
+  @override
+  _MyHomePageState2 createState() => _MyHomePageState2();
+}
 
+class _MyHomePageState2 extends State<MyHomePage2> {
+  List<Movie> movies = [];
+  List<Movie> movies1 = [];
 
+  @override
+  void initState() {
+    getHttp();
+    getHttp1();
 
+    super.initState();
+  }
 
+  void getHttp1() async {
+    var response = await Dio().get(
+      'https://api.themoviedb.org/3/movie/top_rated',
+      queryParameters: {"api_key": "f55fbda0cb73b855629e676e54ab6d8e"},
+    );
 
+    movies1 = (response.data["results"] as List)
+        .map((map) => Movie.fromMap(map))
+        .toList();
+  }
+
+  void getHttp() async {
+    var response = await Dio().get(
+      'https://api.themoviedb.org/3/movie/popular',
+      queryParameters: {"api_key": "f55fbda0cb73b855629e676e54ab6d8e"},
+    );
+
+    movies = (response.data["results"] as List)
+        .map((map) => Movie.fromMap(map))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff261C2C),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Movies App',
+            style: TextStyle(
+                color: Colors.cyanAccent, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Color(0xff3E2C41),
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                InkWell(
+                    child: Text(
+                      'View all..',
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyHomePage()));
+                    }),
+              ],
+            ),
+            Container(
+              height: 300,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: movies.length,
+                  itemBuilder: (context, index) {
+                    Movie movie = movies[index];
+                    print('omar ${movie.title}');
+
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Screen3(
+                                      name: movie.title,
+                                      realsedata: movie.releasedate,
+                                      image: movie.posterpath,
+                                      vote: movie.rate,
+                                      overview: movie.overview,
+                                    )));
+                      },
+                      child: Container(
+                        height: 100,
+                        child: Row(children: [
+                          Container(
+                            child: Column(children: [
+                              Expanded(
+                                child: Image.network(
+                                  'https://image.tmdb.org/t/p/original/${movie.posterpath}',
+                                  height: 100,
+                                ),
+                              ),
+                              Text(
+                                movie.title ?? '',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text(
+                                movie.releasedate ?? '',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ]),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          )
+                        ]),
+                      ),
+                    );
+                  }),
+            ),
+            Container(
+              color: Color(0xff261C2C),
+              height: 300,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: movies1.length,
+                  itemBuilder: (context, index) {
+                    Movie movie = movies1[index];
+                    print('omar ${movie.title}');
+
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Screen3(
+                                      name: movie.title,
+                                      realsedata: movie.releasedate,
+                                      image: movie.posterpath,
+                                      vote: movie.rate,
+                                      overview: movie.overview,
+                                    )));
+                      },
+                      child: Container(
+                        height: 100,
+                        child: Row(children: [
+                          Container(
+                            child: Column(children: [
+                              Expanded(
+                                child: Image.network(
+                                  'https://image.tmdb.org/t/p/original/${movie.posterpath}',
+                                  height: 100,
+                                ),
+                              ),
+                              Text(
+                                movie.title ?? '',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text(
+                                movie.releasedate ?? '',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ]),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          )
+                        ]),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ));
+  }
+}
